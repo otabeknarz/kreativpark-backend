205765c0875ef4a4a2af24b588e36f1524efe5b6
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-02 14:33:51 +0500

    implemented SeatConsumer

--- core/consumers.py
+++ core/consumers.py
@@ -5,57 +5,63 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class SeatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name = 'sections_group'
-        self.user = self.scope['user']
+        self.group_name = "sections_group"
+        self.user = self.scope["user"]
 
         if not self.user.is_authenticated:
             await self.accept()
-            await self.send(text_data=json.dumps({
-                'message': 'You are not authenticated',
-            }))
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        "message": "You are not authenticated",
+                    }
+                )
+            )
             await self.close()
             return
 
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
 
         await self.accept()
-        await self.send(text_data=json.dumps({
-            'message': 'You are connected',
-            'user': self.user.username,
-        }))
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message": "You are connected",
+                    "user": self.user.username,
+                }
+            )
+        )
 
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
         await self.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        seat_id = text_data_json['seat_id']
-        status = text_data_json['status']
+        seat_id = text_data_json["seat_id"]
+        status = text_data_json["status"]
 
         await self.channel_layer.group_send(
             self.group_name,
             {
-                'type': 'chat_message',
-                'seat_id': seat_id,
-                'user': self.user.username,
-                'status': status,
-            }
+                "type": "booking_seat",
+                "seat_id": seat_id,
+                "user": self.user.username,
+                "status": status,
+            },
         )
 
-    async def chat_message(self, event):
-        seat_id = event['seat_id']
-        status = event['status']
-        username = event['user']
+    async def booking_seat(self, event):
+        seat_id = event["seat_id"]
+        status = event["status"]
+        username = event["user"]
 
-        await self.send(text_data=json.dumps({
-            'seat_id': seat_id,
-            'user': username,
-            'status': status,
-        }))
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "seat_id": seat_id,
+                    "user": username,
+                    "status": status,
+                }
+            )
+        )
