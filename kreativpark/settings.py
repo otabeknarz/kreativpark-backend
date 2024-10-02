f6ddc7e40c0c1a2d35385dc75abed0f58ed13c5a
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-03 01:30:55 +0500

    bug fixed

--- kreativpark/settings.py
+++ kreativpark/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 # STATICFILES_DIRS = (BASE_DIR / "static",)
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = str(BASE_DIR / "static")
 
 
 # Media settings
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = str(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
