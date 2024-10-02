7ae074928f454b3532cd7345fa790b5bc2d71e35
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-02 14:35:04 +0500

    set up asgi

--- kreativpark/asgi.py
+++ kreativpark/asgi.py
@@ -5,15 +5,13 @@ from core.routing import websocket_urlpatterns
 from channels.security.websocket import AllowedHostsOriginValidator
 from core.middleware import JWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kreativpark.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kreativpark.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        JWTAuthMiddleware(  # Custom JWT middleware
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            JWTAuthMiddleware(URLRouter(websocket_urlpatterns))  # Custom JWT middleware
+        ),
+    }
+)
