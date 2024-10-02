2200896ae001984d7516d894511e1ab9d1785bf4
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-03 01:24:24 +0500

    deleted unused folder

--- react/src/components/base/Button.jsx
+++
@@ -1,5 +0,0 @@
-import Button from '@mui/material/Button';
-
-export default function ButtonUsage({ text, onClick, onClickProps, color }) {
-  return <Button variant="contained" onClick={() => onClick(onClickProps)} color={color}>{text}</Button>;
-}
