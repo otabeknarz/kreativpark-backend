9181346ecf2b82606ffe0f5493c2939c2c8296dd
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-02 14:34:38 +0500

    wrote middleware to authenticate with JWT token websocket requests

--- core/middleware.py
+++ core/middleware.py
@@ -7,12 +7,13 @@ from django.contrib.auth import get_user_model
 import jwt
 from django.conf import settings
 
+
 @database_sync_to_async
 def get_user_from_token(token):
     try:
         # Decode the token using the SECRET_KEY
         decoded_data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        user_id = decoded_data.get('user_id')
+        user_id = decoded_data.get("user_id")
 
         if user_id:
             return get_user_model().objects.get(id=user_id)
@@ -21,20 +22,21 @@ def get_user_from_token(token):
     except (jwt.ExpiredSignatureError, jwt.DecodeError, get_user_model().DoesNotExist):
         return AnonymousUser()
 
+
 class JWTAuthMiddleware:
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
         # Extract token from query string
-        query_string = parse_qs(scope['query_string'].decode())
-        token = query_string.get('token')
+        query_string = parse_qs(scope["query_string"].decode())
+        token = query_string.get("token")
 
         if token:
             # Fetch user associated with this token asynchronously
-            scope['user'] = await get_user_from_token(token[0])
+            scope["user"] = await get_user_from_token(token[0])
         else:
-            scope['user'] = AnonymousUser()
+            scope["user"] = AnonymousUser()
 
         # Call the inner application (the consumer)
         return await self.inner(scope, receive, send)
