fd8856a514b43e530dfca94a394ab52f93975056
Otabek Narziqulov <otabeknarz@gmail.com>
2024-10-06 03:45:36 +0500

    added daphne to INSTALLED_APPS

--- kreativpark/settings/base.py
+++ kreativpark/settings/base.py
@@ -18,6 +18,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
